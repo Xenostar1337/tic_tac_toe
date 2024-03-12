439a71435f1faece16553f552c6ad003bdb159c3
Xenostar <157074608+Xenostar1337@users.noreply.github.com>
2024-03-11 23:45:15 -0600

    added winner funtionality, names and color selector

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
 const player1 = {
-    Name: "Player 1",
-    Icon: "X"
+    Name: document.getElementById("playerName1").textContent,
+    Icon: "X",
+    Color: 
+        function(){
+        let colorPicker = document.getElementById("colorPicker1");
+        let selectedColor = colorPicker.value;
+        return selectedColor;
+    }
+
+   
 };
- 
+
 const player2 = {
-    Name: "Player 2",  //get names from a form
-    Icon: "O"
+    Name: document.getElementById("playerName1").textContent,  //get names from a form
+    Icon: "O",
+    Color: 
+        function(){
+        let colorPicker = document.getElementById("colorPicker2");
+        let selectedColor = colorPicker.value;
+        return selectedColor;
+    }
 };
 
 let winningColor = "indigo";
@@ -37,7 +51,7 @@ function gameArray() {
 }
 
 function whoseTurn() {   
-    let turn = pointer % 2 === 1 ? "X" : "O";
+    let turn = pointer % 2 === 1 ? player1.Icon : player2.Icon;
     return turn;
 }
 
@@ -46,107 +60,159 @@ function isWinner() {
     let winner1;
     let winner2;
     let winner3;
-    function win(){ 
-    winner1.style.backgroundColor = winningColor;
+    let jee = document.getElementById("playerName1").value;
+    let joo = document.getElementById("playerName2").value;
+    let results;
+
+    function win(){     
+    winner1.style.backgroundColor = winningColor;  
     winner2.style.backgroundColor = winningColor;
     winner3.style.backgroundColor = winningColor;
+    winner1.style.animation = "spin 3s linear infinite, flash .5s ease-in-out infinite";
+    winner2.style.animation = "spin 3s linear infinite, flash .5s ease-in-out infinite";
+    winner3.style.animation = "spin 3s linear infinite, flash .5s ease-in-out infinite";
+    document.getElementById("header").style.color = "lime";
+    document.getElementById("header").textContent = results;
+    document.getElementById("status").style.display = "grid";
+    document.getElementById("status").textContent = "PLAY AGAIN?";    
     }
    
     if (bap[0][0] !== 0 && bap[0][0] ==  bap[0][1] &&  bap[0][0] ==  bap[0][2]) {
         winner1 = document.getElementById("a0b0"); 
         winner2 = document.getElementById("a0b1");
         winner3 = document.getElementById("a0b2");  
+        if (winner1.textContent == player1.Icon) {
+            results = "Winner " + jee; 
+        }
+        if (winner1.textContent == player2.Icon) {
+            results = "Winner " + joo;
+        }
         win(); 
-        return "Winner";
+        
     }
     if (bap[0][0] !== 0 && bap[0][0] ==  bap[1][0] &&  bap[0][0] ==  bap[2][0]) {
         winner1 = document.getElementById("a0b0"); 
         winner2 = document.getElementById("a1b0");
         winner3 = document.getElementById("a2b0"); 
+        if (winner1.textContent == player1.Icon) {
+            results = "Winner " + jee; 
+        }
+        if (winner1.textContent == player2.Icon) {
+            results = "Winner " + joo;
+        }  
         win(); 
-        return "Winner";    
+          
     }
     if (bap[0][0] !== 0 && bap[0][0] ==  bap[1][1] &&  bap[0][0] ==  bap[2][2]) {
         winner1 = document.getElementById("a0b0"); 
         winner2 = document.getElementById("a1b1");
         winner3 = document.getElementById("a2b2"); 
+        if (winner1.textContent == player1.Icon) {
+            results = "Winner " + jee; 
+        }
+        if (winner1.textContent == player2.Icon) {
+            results = "Winner " + joo;
+        } 
         win(); 
-        return "Winner"; 
+        
     }
     if (bap[2][0] !== 0 && bap[2][0] ==  bap[2][1] &&  bap[2][0] ==  bap[2][2]) {
         winner1 = document.getElementById("a2b0"); 
         winner2 = document.getElementById("a2b1");
         winner3 = document.getElementById("a2b2");  
+        if (winner1.textContent == player1.Icon) {
+            results = "Winner " + jee; 
+        }
+        if (winner1.textContent == player2.Icon) {
+            results = "Winner " + joo;
+        }  
         win(); 
-        return "Winner";   
+         
     }
     if (bap[2][0] !== 0 && bap[2][0] ==  bap[1][1] &&  bap[2][0] ==  bap[0][2]) {
         winner1 = document.getElementById("a2b0"); 
         winner2 = document.getElementById("a1b1");
         winner3 = document.getElementById("a0b2"); 
+        if (winner1.textContent == player1.Icon) {
+            results = "Winner " + jee; 
+        }
+        if (winner1.textContent == player2.Icon) {
+            results = "Winner " + joo;
+        } 
         win();  
-        return "Winner";           
+                  
     }
     if (bap[0][2] !== 0 && bap[0][2] ==  bap[1][2] &&  bap[0][2] ==  bap[2][2]) {
         winner1 = document.getElementById("a0b2"); 
         winner2 = document.getElementById("a1b2");
         winner3 = document.getElementById("a2b2"); 
+        
+        if (winner1.textContent == player1.Icon) {
+            results = "Winner " + jee; 
+        }
+        if (winner1.textContent == player2.Icon) {
+            results = "Winner " + joo;
+        } 
         win(); 
-        return "Winner";   
+        
     }
     if (bap[0][1] !== 0 && bap[0][1] ==  bap[1][1] &&  bap[0][1] ==  bap[2][1]) {
         winner1 = document.getElementById("a0b1"); 
         winner2 = document.getElementById("a1b1");
         winner3 = document.getElementById("a2b1"); 
+           
+        if (winner1.textContent == player1.Icon) {
+            results = "Winner " + jee; 
+        }
+        if (winner1.textContent == player2.Icon) {
+            results = "Winner " + joo;
+        }
         win();  
-        return "Winner";   
+        
     }
     if (bap[1][0] !== 0 && bap[1][0] ==  bap[1][1] &&  bap[1][0] ==  bap[1][2]) {
         winner1 = document.getElementById("a1b0"); 
         winner2 = document.getElementById("a1b1");
         winner3 = document.getElementById("a1b2");
+        if (winner1.textContent == player1.Icon) {
+            results = "Winner " + jee; 
+        }
+        if (winner1.textContent == player2.Icon) {
+            results = "Winner " + joo;
+        }
         win(); 
-        return "Winner";  
+        
+         
+        
     }
     if (pointer > 9) {
-            return "DRAW";
+            results = "DRAW";
         }
 }
 
-function results() {
-    let result = isWinner();
-    if (result !== undefined) {
-    document.getElementById("header").style.color = "lime";
-    document.getElementById("header").textContent = result;
-    document.getElementById("status").style.display = "grid";
-    document.getElementById("status").textContent = "PLAY AGAIN?";
-    }
-}
 
 function gameState() {
     let row;
     let col;
     let cellValue;
     //if (whoseTurn() == "O" && opponent == "ai") {AIturn();} 
-
     document.querySelectorAll('.card').forEach(card => {          
         for ( row = 0; row < game.getBoard().length; row++) {
             for ( col = 0; col < game.getBoard()[row].length; col++) {
                  cellValue = game.getBoard()[row][col];
                 if (row == card.id[1] && col == card.id[3] && cellValue !== 0) {                    
                     card.textContent = cellValue;       
-                    if(card.textContent == "O") {
-                        card.style.color = "magenta";
+                    if(card.textContent == player1.Icon) {
+                        card.style.color = player1.Color();
                     }
-                    if(card.textContent == "X") {
-                        card.style.color = "orange";
+                    if(card.textContent == player2.Icon) {
+                        card.style.color = player2.Color();
                     }                    
                 }
             }   
         }        
     });
-    result = results();
-   
+    isWinner();
 }        
 
 function AIturn(){
@@ -158,6 +224,7 @@ function newGame(button) {
     document.querySelectorAll('.card').forEach(card => {
         card.textContent = '';
         card.style.backgroundColor = "black";
+        card.style.animation = "none";
         document.getElementById("status").style.display = "none";
       });
     game = gameArray();  
@@ -167,7 +234,8 @@ function newGame(button) {
     foo.style.display = "none";
     document.getElementById('header').textContent = 'TIC TAC TOE';
     document.querySelector(".board").style.display = "grid"; 
-    document.getElementById('header').style.color = 'aqua';    
+    document.getElementById('header').style.color = 'aqua';  
+    document.querySelector(".playerBox").style.display = "none";
     gameState();
 }  
 
